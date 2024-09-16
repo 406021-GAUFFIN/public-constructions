b8cd00523e5b52fd4c6dc3f391b05754d3376e01
Genaro Richard <113908@tecnicatura.frc.utn.edu.ar>
2024-09-16 13:04:20 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,16 @@
 -- Insert data into DOCUMENTATION_TYPE
-INSERT INTO DOCUMENTATION_TYPE (id, name, description) VALUES (1,'Architectural Plan', 'Plan of the construction project');
-INSERT INTO DOCUMENTATION_TYPE (id, name, description) VALUES (2,'Insurance Info', 'Insurance details related to the construction');
+INSERT INTO DOCUMENTATION_TYPE (id, name, description) VALUES (1, 'Plano Arquitectónico', 'Planos del proyecto de construcción');
+INSERT INTO DOCUMENTATION_TYPE (id, name, description) VALUES (2, 'Informacion de Seguro', 'Detalles del seguro relacionado con la construcción');
 
--- Insert data into WORKER_SPECIALITY_TYPE
-INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (1, 'Electrician', 'Electrician with certification');
-INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (2, 'Plumber', 'Plumber with certification');
-INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (3, 'Surveyor', 'Surveyor with certification');
-INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (4, 'Quantity Surveyor', 'Quantity Surveyor with certification');
-INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (5, 'Interior Designer', 'Interior Designer with certification');
-INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (6, 'Landscape Architect', 'Landscape Architect with certification');
-INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (7, 'Other', 'Other specialty');
+-- Insert specialties with names and descriptions in Spanish
+INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (1, 'Electricista', 'Electricista con certificación');
+INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (2, 'Fontanero', 'Fontanero con certificación');
+INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (3, 'Topografo', 'Topógrafo especializado en levantamientos y mediciones');
+INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (4, 'Perito de Cantidades', 'Perito especializado en medición de obras y costos');
+INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (5, 'Diseñador de Interiores', 'Diseñador de interiores especializado en planificación y decoración de espacios');
+INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (6, 'Arquitecto Paisajista', 'Arquitecto paisajista especializado en diseño de espacios exteriores');
+INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (7, 'Otro', 'Otro tipo de especialidad no categorizada');
+INSERT INTO WORKER_SPECIALITY_TYPE (id, name, description) VALUES (8, 'Albañil', 'Trabajador especializado en labores manuales pesadas en sitios de construcción');
+
 
 
