85851f53db3d0fe4ff2cf4da9f0fe16f93ea0d96
Alejandro Ziade <109633897+405141-Ziade-Alejandro@users.noreply.github.com>
2024-09-22 21:34:18 -0300

    _audit fail _aud is champion

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/Application.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/Application.java
@@ -3,6 +3,9 @@ package ar.edu.utn.frc.tup.lc.iv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import java.util.Properties;
 
 /**
  * Main class.
@@ -16,6 +19,13 @@ public class Application {
      * @param args application args
      */
     public static void main(String[] args) {
+//        Properties hibernateProperties = new Properties();
+//
+//        hibernateProperties.setProperty(
+//                "org.hibernate.envers.audit_table_suffix","_audit"
+//        );
+
+
         SpringApplication.run(Application.class, args);
     }
 }
