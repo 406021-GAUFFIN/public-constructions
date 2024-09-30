e6a02ddd745f05445bbef03dbd811b493eb844c6
405312-Benegas-Joaquin <164806157+405312-Benegas-Joaquin@users.noreply.github.com>
2024-09-30 13:14:00 -0300

    fix: add periods at the end of each comment

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/controllers/api_response/ApiResponseConstants.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/controllers/api_response/ApiResponseConstants.java
@@ -6,77 +6,77 @@ package ar.edu.utn.frc.tup.lc.iv.controllers.api_response;
 public final class ApiResponseConstants {
 
     /**
-     * OK response code
+     * OK response code.
      */
     public static final String OK = "200";
 
     /**
-     * Created response code
+     * Created response code.
      */
     public static final String CREATED = "201";
 
     /**
-     * No content response code
+     * No content response code.
      */
     public static final String NO_CONTENT = "204";
 
     /**
-     * Internal server error response code
+     * Internal server error response code.
      */
     public static final String INTERNAL_SERVER_ERROR = "500";
 
     /**
-     * Bad request response code
+     * Bad request response code.
      */
     public static final String BAD_REQUEST = "400";
 
     /**
-     * Not found response code
+     * Not found response code.
      */
     public static final String NOT_FOUND = "404";
 
     /**
-     * Conflict response code
+     * Conflict response code.
      */
     public static final String CONFLICT = "409";
 
     /**
-     * Success message
+     * Success message.
      */
     public static final String OK_MESSAGE = "Operation completed successfully";
 
     /**
-     * Resource created message
+     * Resource created message.
      */
     public static final String CREATED_MESSAGE = "Resource created successfully";
 
     /**
-     * Resource deleted message
+     * Resource deleted message.
      */
     public static final String NO_CONTENT_MESSAGE = "Resource deleted successfully";
 
     /**
-     * Internal server error message
+     * Internal server error message.
      */
     public static final String INTERNAL_SERVER_ERROR_MESSAGE = "Internal server error";
 
     /**
-     * Bad request message
+     * Bad request message.
      */
     public static final String BAD_REQUEST_MESSAGE = "Invalid request";
 
     /**
-     * Not found message
+     * Not found message.
      */
     public static final String NOT_FOUND_MESSAGE = "Resource not found";
 
     /**
-     * Conflict message
+     * Conflict message.
      */
     public static final String CONFLICT_MESSAGE = "Resource already exists";
 
     /**
-     * Private constructor
+     * Private constructor.
      */
     private ApiResponseConstants() {
     }
