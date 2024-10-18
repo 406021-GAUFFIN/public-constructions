87d7860b32efef5ed672d35076711f14157ffe28
“114230-RAPACCIONI” <114230@tecnicatura.frc.utn.edu.ar>
2024-10-17 23:16:07 -0300

    Update ConstructionUpdateDto.java

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/construction/ConstructionUpdateDto.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/construction/ConstructionUpdateDto.java
@@ -1,6 +1,8 @@
 package ar.edu.utn.frc.tup.lc.iv.dtos.construction;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,28 +21,33 @@ public class ConstructionUpdateDto {
      * Planned start date of the construction.
      */
     @JsonProperty("planned_start_date")
+    @NotNull(message = "Planned start date cannot be null")
     private Date plannedStartDate;
 
     /**
      * Planned end date of the construction.
      */
     @JsonProperty("planned_end_date")
+    @NotNull(message = "Planned end date cannot be null")
     private Date plannedEndDate;
 
     /**
      * Description or details about the construction project.
      */
+    @NotBlank(message = "Description cannot be null or empty")
     private String description;
 
     /**
      * Name of the construction project.
      */
+    @NotBlank(message = "Project name cannot be null or empty")
     @JsonProperty("project_name")
     private String projectName;
 
     /**
      * Address of the construction project.
      */
+    @NotBlank(message = "Address cannot be null or empty")
     @JsonProperty("project_address")
     private String projectAddress;
 }
